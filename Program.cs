3ac7b922e13d189ee4715777c05f5c87bf40f710
Roxi <ganieva0114@gmail.com>
2023-10-30 14:27:58 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FortuneTeller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("PLease enter your question: ");
+                Console.ReadLine();
+
+                Random yesNoMaybe = new Random();
+                int answerNum = yesNoMaybe.Next(1, 4);
+
+                if (answerNum == 1)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else if (answerNum == 2)
+                {
+                    Console.WriteLine("Maybe");
+                }
+                else if (answerNum == 3)
+                {
+                    Console.WriteLine("No");
+                }
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
